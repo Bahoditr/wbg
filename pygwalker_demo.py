9c58b65dfca90558672189be4cb194a5ae54e6c1
Bahoditr <165786166+Bahoditr@users.noreply.github.com>
2024-04-03 10:31:17 +0500

    Update pygwalker_demo.py

--- pygwalker_demo.py
+++ pygwalker_demo.py
@@ -13,7 +13,7 @@ st.set_page_config(
 st.title("Use Pygwalker In Streamlit")
 
 # Import your data
-df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
+df = pd.read_excel("https://raw.githubusercontent.com/Bahoditr/wbg/03ec3936577188897d8a805121e397cb779f392a/raw_data.xlsx")
 
 # Generate the HTML using Pygwalker
 pyg_html = pyg.to_html(df)
