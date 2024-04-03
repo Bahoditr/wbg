69b7e8e6f4dcca0989d86b31076446eab5aab7b6
Bahoditr <165786166+Bahoditr@users.noreply.github.com>
2024-04-03 10:32:41 +0500

    Update wbg.py

--- wbg.py
+++ wbg.py
@@ -2,21 +2,21 @@ import pygwalker as pyg
 import pandas as pd
 import streamlit.components.v1 as components
 import streamlit as st
- 
+
 # Adjust the width of the Streamlit page
 st.set_page_config(
     page_title="Use Pygwalker In Streamlit",
     layout="wide"
 )
- 
+
 # Add Title
 st.title("Use Pygwalker In Streamlit")
- 
+
 # Import your data
 df = pd.read_excel("https://raw.githubusercontent.com/Bahoditr/wbg/03ec3936577188897d8a805121e397cb779f392a/raw_data.xlsx")
- 
+
 # Generate the HTML using Pygwalker
 pyg_html = pyg.to_html(df)
- 
+
 # Embed the HTML into the Streamlit app
 components.html(pyg_html, height=1000, scrolling=True)
